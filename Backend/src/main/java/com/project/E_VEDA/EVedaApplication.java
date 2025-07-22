18d05965066df538dee9bfac8ef643b58c7514d6
Ansh sharma <ansharma013@gmail.com>
2025-07-22 17:22:24 +0530

    Backend Initialized

---
+++ Backend/src/main/java/com/project/E_VEDA/EVedaApplication.java
@@ -0,0 +1,13 @@
+package com.project.E_VEDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVedaApplication.class, args);
+	}
+
+}
