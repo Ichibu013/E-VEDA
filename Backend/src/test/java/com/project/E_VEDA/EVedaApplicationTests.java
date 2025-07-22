7bcfc8d2885341aebcb04c576ee3b16de33197d1
Ansh sharma <ansharma013@gmail.com>
2025-07-22 17:22:24 +0530

    Backend Initialized

---
+++ Backend/src/test/java/com/project/E_VEDA/EVedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.E_VEDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
