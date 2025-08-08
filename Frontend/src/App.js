ebac7bb3ad46af65274821882e6aa9961ebeb942
Ansh sharma <98533444+Ichibu013@users.noreply.github.com>
2025-08-08 15:01:53 +0530

    Frontend shreeyash (#13)
    Home Page Done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // All imports at the top
-import WelcomePage from './WelcomePage';
-import LoginPage from './LoginPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LoginPage from './LoginPage';
+import WelcomePage from './Components/WelcomePage';
+import HomePage from './Components/HomePage';
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<WelcomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            {/* Add more routes here if needed */}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      {/* <Route path="/login" element={<LoginPage />} /> */}
+      <Route path="/homepage" element={<HomePage />} />
+
+      {/* Add more routes here if needed */}
+    </Routes>
+  );
 }
 
-// RootApp wraps the main App component with BrowserRouter
 function RootApp() {
-    return (
-        <Router>
-            <App />
-        </Router>
-    );
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
 }
 
 export default RootApp;
