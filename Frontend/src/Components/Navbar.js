4e27bbe2238d69f0b12952ebc89a300165366e15
IScreates <shreeyashshinde76@gmail.com>
2025-08-08 22:04:15 +0530

    navbar updated

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -1,1 +1,64 @@
-n;
+// Navbar.jsx
+import React from 'react';
+import '../Styles/HomePage.css';
+import { useNavigate } from 'react-router-dom';
+import logo from '../Assets/bg remove logo.png';
+import menuIcon from '../Assets/ham-menu-icon.png';
+import closeIcon from '../Assets/close-icon.png';
+// import LoginPage from '../Styles/LoginPage.css';
+
+function Navbar({ isNavbarOpen, toggleNavbar }) {
+  const navigate = useNavigate();
+
+  const handleProceedClick = () => {
+    navigate('/Login');
+  };
+  return (
+    <nav className="navbar container flex">
+      <>
+        <a href="#home" className="navbar-brand">
+          <img src={logo} alt="Logo" />
+        </a>
+        <button className="hamburger" onClick={toggleNavbar}>
+          <img src={isNavbarOpen ? closeIcon : menuIcon} alt="menu" />
+        </button>
+      </>
+      <div className={`navbar-collapse ${isNavbarOpen ? 'open' : ''}`}>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="#home" className="nav-link" onClick={toggleNavbar}>
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#about" className="nav-link" onClick={toggleNavbar}>
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#doc-panel" className="nav-link" onClick={toggleNavbar}>
+              Doctors
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#contact" className="nav-link" onClick={toggleNavbar}>
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#login" className="nav-link" onClick={handleProceedClick}>
+              Login
+            </a>
+          </li>
+          {/* <li className="nav-item">
+            <a href="#signup" className="nav-link " onClick={toggleNavbar}>
+              Sign Up
+            </a>
+          </li> */}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
